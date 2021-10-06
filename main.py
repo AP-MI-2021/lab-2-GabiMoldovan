233060b306aa720885f3ce3d9a3a9254dcde0c28
GabiMoldovan <moldovan.gabi75@gmail.com>
2021-10-06 14:41:55 +0300

    Modificari aduse programului in urma participarii la ora de seminar

--- main.py
+++ main.py
@@ -1,37 +1,66 @@
 def get_goldbach(n):
+    '''
+    Determina modalitatea de scriere a lui n ca suma de doua numere prime
+    :param n: nr natural
+    :return: doua numere p1, p2 ( ambele prime ) cu proprietatea ca n = p1 + p2
+    '''
     # Pentru a avea o solutie, parametrul dat trebuie sa respecte conditiile: n > 3 si n % 2 == 0 ( sa fie par )
     for i in range(int(n), 1, -1):
-        if test_get_goldbach(i):
+        if prime(i):
             for j in range(3, int(i)+1, 2):
-                if test_get_goldbach(j) and int(i)+int(j)==int(n):
-                    print(i, end = " ")
-                    print(j)
-                    return
-    print("NU SE VERIFICA")
+                if prime(j) and int(i)+int(j)==int(n):
+                    return i, j
+    return -1, -1
 
-def test_get_goldbach(n):
+def prime(n):
+    '''
+    Determina primalitatea lui n
+    :param n: nr natural
+    :return: True daca n este prim, altfel False
+    '''
     if int(n) < 2: return False
     for i in range(2, int(n) // 2 + 1):
         if int(n) % int(i) == 0: return False
     return True
 
+def test_get_goldbach():
+    assert get_goldbach(6) == (3, 3)
+    assert get_goldbach(7) == (-1, -1)
+    assert get_goldbach(256) == (251, 5)
+
+def test_x0(root, x, x0):
+    if float(abs(float(root) - float(x))) < float(x0):
+        return True
+    return False
+
 def get_newton_sqrt(n, steps):
+    '''
+    Calculeaza radical din n folosind steps pasi
+    Pentru un rezultat cat mai aproape de sqrt(n), se seteaza x0 sa fie cat mai mic ( ex: 0.000001 )
+    :param n: nr natural
+    :param steps: nr natural
+    :return: sqrt(n)
+    '''
     x=n
     x0=2
     while steps:
         steps=int(steps)-1
         root=float(0.5)*(float(x)+float(n)/float(x))
-        if test_get_newton_sqrt(root, x, x0) == True:
+        if test_x0(root, x, x0) == True:
             break
         x=float(root)
     return float(root)
 
-def test_get_newton_sqrt(root, x, x0):
-    if float(abs(float(root) - float(x))) < float(x0):
-        return True
-    return False
+def test_get_newton_sqrt():
+    assert get_newton_sqrt(100, 10) == 10.032578510960604
+    assert get_newton_sqrt(6, 3) == 2.607142857142857
+
 
 if __name__ == '__main__':
+
+    test_get_newton_sqrt()
+    test_get_goldbach()
+
     while True:
         print("Optiuni:")
         print("1. Conjunctura lui Goldbach")
@@ -41,7 +70,11 @@ if __name__ == '__main__':
 
         if option == "1":
             n = input("Introdu numarul: ")
-            get_goldbach(n)
+            p1, p2 = get_goldbach(n)
+            if p1 == -1:
+                print("NU SE VERIFICA!")
+            else:
+                print(str(n) + " = " + str(p1) + " + " + str(p2))
 
         if option == "2":
             n = input("Introdu numarul: ")
